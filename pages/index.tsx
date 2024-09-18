8a34d89c4ca4934e9de5eb1bf003d3db5d9cc6af
Victor <victor.briez@epitech.eu>
2024-09-19 01:10:29 +0200

    init(all): init projects with default files

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import { Link } from "@nextui-org/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-xl text-center justify-center">
+          <span className={title()}>Élevez vos&nbsp;</span>
+          <span className={title({ color: "violet" })}>streams&nbsp;</span>
+          <span className={title()}>au niveau supérieur.&nbsp;</span>
+          <div className={subtitle({ class: "mt-4" })}>
+            Transformer chaque stream en une expérience unique grâce à des
+            overlays dynamiques et facilement personnalisables.
+          </div>
+        </div>
+
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href={siteConfig.links.docs}
+          >
+            Découvrir
+          </Link>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+          >
+            <GithubIcon size={20} />
+            GitHub
+          </Link>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
